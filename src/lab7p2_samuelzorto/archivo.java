a3c044fa2e69e3a595590990725681531de593eb
szorto <samuelzorto@gmail.com>
2023-03-10 14:07:13 -0600

    first commit

---
+++ src/lab7p2_samuelzorto/archivo.java
@@ -0,0 +1,55 @@
+package lab7p2_samuelzorto;
+
+public class archivo {
+    private String nombre, link, extension;
+    private double tamanio;
+
+    public archivo() {
+    }
+
+    public archivo(String nombre, String link, String extension, double tamanio) {
+        this.nombre = nombre;
+        this.link = link;
+        this.extension = extension;
+        this.tamanio = tamanio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public double getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    @Override
+    public String toString() {
+        return "archivo{" + "nombre=" + nombre + ", link=" + link + ", extension=" + extension + ", tamanio=" + tamanio + '}';
+    }
+    
+    
+}
---
+++ src/lab7p2_samuelzorto/carpeta.java
@@ -0,0 +1,56 @@
+package lab7p2_samuelzorto;
+
+import java.util.ArrayList;
+
+public class carpeta {
+    private String nombre, link;
+    private ArrayList<archivo> arc = new ArrayList();
+    private ArrayList<carpeta> car = new ArrayList();
+
+    public carpeta() {
+    }
+
+    public carpeta(String nombre, String link) {
+        this.nombre = nombre;
+        this.link = link;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public ArrayList<archivo> getArc() {
+        return arc;
+    }
+
+    public void setArc(ArrayList<archivo> arc) {
+        this.arc = arc;
+    }
+
+    public ArrayList<carpeta> getCar() {
+        return car;
+    }
+
+    public void setCar(ArrayList<carpeta> car) {
+        this.car = car;
+    }
+
+    @Override
+    public String toString() {
+        return "carpeta{" + "nombre=" + nombre + ", link=" + link + ", arc=" + arc + ", car=" + car + '}';
+    }
+    
+    
+}
